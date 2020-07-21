6aeaeb308485f98254478e6d9b3b9c8e3ef8556d
FRONT LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGFR2@OOCL.COM>
2020-07-21 23:16:21 +0800

    feature: add printReceipt function

--- main.js
+++ main.js
@@ -1,15 +1,141 @@
+const itemsDatabase = [
+    {
+       barcode: 'ITEM000000',
+       name: 'Coca-Cola',
+       price: 3
+     },
+     {
+       barcode: 'ITEM000001',
+       name: 'Sprite',
+       price: 3
+     },
+     {
+       barcode: 'ITEM000002',
+       name: 'Apple',
+       price: 5
+     },
+     {
+       barcode: 'ITEM000003',
+       name: 'Litchi',
+       price: 15
+     },
+     {
+       barcode: 'ITEM000004',
+       name: 'Battery',
+       price: 2
+     },
+     {
+       barcode: 'ITEM000005',
+       name: 'Instant Noodles',
+       price: 4
+     }
+ ];
+ const barcodes = [
+    'ITEM000000',
+    'ITEM000000',
+    'ITEM000000',
+    'ITEM000000',
+    'ITEM000000',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000004'
+];
+// printReceipt(barcodes);
 function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+//     console.log(`
+// ***<store earning no money>Receipt ***
+// Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
+// Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
+// Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
+// ----------------------
+// Total: 23 (yuan)
+// **********************`)
+
+    cartItems = groupItemsWithCount(barcodes);
+    cartItemsDetail = getItemsDetail(cartItems, itemsDatabase);
+    cartItemsDetailWithTotalPrice = getItemsDetailWithTotalPrice(cartItemsDetail);
+    totalPrice = getCartItemsTotalPrice(cartItemsDetailWithTotalPrice);
+    var receipt = formatReceipt(cartItemsDetailWithTotalPrice, totalPrice);
+    console.log(receipt);
+
+}
+
+function groupItemsWithCount(barcodes){
+    var itemsList = {};
+    var cartItems = [];
+    for (let i=0; i<barcodes.length; i++){
+        if (itemsList[barcodes[i]]==null){
+            var newItem = {
+                barcode: barcodes[i],
+                quantity: 1
+            }
+            itemsList[barcodes[i]] = newItem;
+        }else{
+            itemsList[barcodes[i]].quantity++;
+        }
+    }
+    for (var barcode in itemsList){
+        cartItems.push(itemsList[barcode]);
+    }
+    return cartItems;
+}
+
+function getItemsDetail(cartItems, itemsDatabase){
+    var cartItemsDetail = [];
+    cartItems.forEach(itemInCart => {
+        itemsDatabase.forEach(itemInDatabase => {
+            if (itemInCart.barcode == itemInDatabase.barcode){
+                var itemDetail = {
+                    barcode: itemInCart.barcode,
+                    name: itemInDatabase.name,
+                    unitPrice: itemInDatabase.price,
+                    quantity: itemInCart.quantity
+                }
+                cartItemsDetail.push(itemDetail);
+            }
+        })
+    });
+    return cartItemsDetail;
+}
+
+function getItemsDetailWithTotalPrice(cartItemsDetail){
+    var cartItemsDetailWithTotalPrice = [];
+    cartItemsDetail.forEach(itemInCart=>{
+        var itemTotalPrice = itemInCart.quantity * itemInCart.unitPrice;
+        var itemDetailWithTotalPrice = {
+            barcode: itemInCart.barcode,
+            name: itemInCart.name,
+            unitPrice: itemInCart.unitPrice,
+            quantity: itemInCart.quantity,
+            totalPrice: itemTotalPrice
+        }
+        cartItemsDetailWithTotalPrice.push(itemDetailWithTotalPrice);
+    })
+    return cartItemsDetailWithTotalPrice;
+} 
+
+function getCartItemsTotalPrice(cartItemsDetailWithTotalPrice){
+    var totalPrice = 0;
+    cartItemsDetailWithTotalPrice.forEach(item => {
+        totalPrice += item.totalPrice;
+    })
+    return totalPrice;
+}
+
+function formatReceipt(cartItemsDetailWithTotalPrice, totalPrice){
+    var receipt = '\n***<store earning no money>Receipt ***\n';
+    cartItemsDetailWithTotalPrice.forEach(item => {
+        var itemDescription = `Name: ${item.name}, Quantity: ${item.quantity}, Unit price: ${item.unitPrice} (yuan), Subtotal: ${item.totalPrice} (yuan)`;
+        receipt += itemDescription + '\n';
+    })
+    receipt += '----------------------\n';
+    receipt += `Total: ${totalPrice} (yuan)\n`;
+    receipt += '**********************';
+    return receipt;
 }
 
 
 module.exports = {
     printReceipt
 };
+
---
+++ tasking.md
@@ -0,0 +1,68 @@
+### Tasking
+1. 统计barcodes中每种item数量 groupItemsWithCount
+   - 输入：barcodes: [barcode:string ]
+   - 输出：cartItems: [{barcode:string, quantity: int}]
+2. 获取每个item的信息 getItemsDetail
+   - 输入：
+      - cartItems: [{barcode:string, quantity: int}]
+      - itemsDetail: [{barcode:string, name:string, price:int}]
+   - 输出：cartItemsDetail:[{barcode:string, name:string, quantity:int, unitPrice:int}]
+3. 计算每种item的总价 getItemsDetailWithTotalPrice
+   - 输入：cartItemsDetail:[{barcode:string, name:string, quantity:int, unitPrice:int}]
+   - 输出：cartItemsDetailWithTotalPrice:[{barcode:string, name:string, quantity:int, unitPrice:int, totalPrice:int}]
+4. 计算所有商品的总价 getCartItemsTotalPrice
+   - 输入：cartItemsDetailWithTotalPrice:[{barcode:string, name:string, quantity:int, unitPrice:int, totalPrice:int}]
+   - 输出：totalPrice：int
+5. 格式化现有数据 formatReceipt
+   - 输入：
+     - cartItemsDetailWithTotalPrice:[{barcode:string, name:string, quantity:int, unitPrice:int, totalPrice:int}]
+     - totalPrice: int
+   - 输出：receipt: string
+6. 输出数据
+   - 输入: receipt:string
+   - 输出: ouput to console
+  
+#### Context map
+![](./pos-machine.png)
+
+#### PDCA
+
+1. 
+| | groupItemsWithCount |
+---|---
+Plan | 8min
+Do | 15min
+Check| 对javascript对象或数组的一些api不熟悉, 思考如何遍历统计比较优
+Action| 去查找js关于对象或数组的api文档，练习一下
+
+2. 
+| | getItemsDetail |
+---|---
+Plan | 8min
+Do | 11min
+Check| 在考虑除了使用双重循环结构，是否还有别的更优的方法来将购物车上的商品匹配到数据库中商品的详情
+Action| 思考一下更优的方法
+
+3. 
+| | getItemsDetailWithTotalPrice |
+---|---
+Plan | 5min
+Do | 5min
+Check| 在构造新的item对象时候有一些字段名写错
+Action| 尽量使用复制的方式
+
+4. 
+| | getCartItemsTotalPrice |
+---|---
+Plan | 3min
+Do | 2min
+Check| 通过遍历数组累加求所有商品总价，这里没遇到障碍
+Action| none
+
+5. 
+| | formatReceipt |
+---|---
+Plan | 8min
+Do | 8min
+Check| 这一步容易遗漏一些需要输出的内容以及一些空格之类的字符
+Action| 多使用\`${}\` 这种模板字符串，要注意输出案例的各种符号如换行符
